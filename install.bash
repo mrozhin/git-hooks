1daea6fa2eff22de2beae6fa09f4f3b5a00fc608
Mikhail Rozhin <rozhinma@gmail.com>
2013-12-08 17:34:01 +0400

    add .git dir support

--- install.bash
+++ install.bash
@@ -6,26 +6,27 @@ git_hooks_dir=$(pwd)
 all_hooks=$(find -type f);
 
 cd ../../
-gitroot=$(git rev-parse --show-cdup)
+git_root=$(git rev-parse --show-cdup)
 
-if [ "${gitroot}" != "" ]; then
-  cd ${gitroot};
+if [ "${git_root}" != "" ]; then
+  cd ${git_root};
 fi
 
 if [ -d .git ]; then
   echo "TODO";
-  exit 1;
+  cd ./.git/hooks/
+else
+  if [ -f .git ]; then
+    hooks_dir=$(cat  ".git" | sed -e "s%gitdir:[ \t]*%%" -e "/^$/d" -e "s%$%/hooks/%")
+    cd ${hooks_dir}
+  fi
 fi
 
-if [ -f .git ]; then
-  hooks_dir=$(cat  ".git" | sed -e "s%gitdir:[ \t]*%%" -e "/^$/d" -e "s%$%/hooks/%")
-  cd $hooks_dir
-  hooks_dir_absolute=$(pwd)
-  relative_path=$(python -c "import os.path; print os.path.relpath('"$git_hooks_dir"', '"$hooks_dir_absolute"')")
-  echo "making links: "$all_hooks
-  echo $all_hooks" " | while read -d " " hook; do
-    full_path=$relative_path"/"$hook
-    echo "ln -s "$full_path";"
-    ln -s $full_path .
-  done
-fi
+hooks_dir_absolute=$(pwd)
+relative_path=$(python -c "import os.path; print os.path.relpath('"${git_hooks_dir}"', '"${hooks_dir_absolute}"')")
+echo "making links: "${all_hooks}
+echo ${all_hooks}" " | while read -d " " hook; do
+  full_path=${relative_path}"/"${hook}
+  echo "ln -s "${full_path}";"
+  ln -s ${full_path} .
+done
