d5e433bc09624884009b789b73fbc5afb5c53eec
Mikhail Rozhin <rozhinma@gmail.com>
2013-12-08 11:27:16 +0400

    mklink for multiple hooks

--- install.bash
+++ install.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
+#set -e;
+
+cd hooks;
 git_hooks_dir=$(pwd)
-cd ../
+all_hooks=$(find -type f);
+
+cd ../../
 gitroot=$(git rev-parse --show-cdup)
 
 if [ "${gitroot}" != "" ]; then
@@ -13,13 +18,14 @@ if [ -d .git ]; then
 fi
 
 if [ -f .git ]; then
-  tmpfile_name=git_hooks.tmp.bash
   hooks_dir=$(cat  ".git" | sed -e "s%gitdir:[ \t]*%%" -e "/^$/d" -e "s%$%/hooks/%")
   cd $hooks_dir
   hooks_dir_absolute=$(pwd)
   relative_path=$(python -c "import os.path; print os.path.relpath('"$git_hooks_dir"', '"$hooks_dir_absolute"')")
-  hook=$relative_path"/hooks/hook" 
-  echo $hook
-  ln -s $hook .
-  #rm $tmpfile
+  echo "making links: "$all_hooks
+  echo $all_hooks" " | while read -d " " hook; do
+    full_path=$relative_path"/"$hook
+    echo "ln -s "$full_path";"
+    ln -s $full_path .
+  done
 fi
